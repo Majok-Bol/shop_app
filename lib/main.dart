e262602bbc8fdd959c96b7ba0459b5a3820c65d2
Majok-Bol <bolmajok001@gmail.com>
2025-03-20 10:48:17 +0300

    function to add and remove items from the list

--- lib/main.dart
+++ lib/main.dart
@@ -17,4 +17,25 @@ class ShoppingList extends StatefulWidget{
 class ShopppingListState extends State<ShoppingList>{
   //create empty list to store the shopping items
   final List<String>itemsToAdd=[];
+  //manage the text editing controller
+  final TextEditingController itemController=TextEditingController();
+  //function to add item to list
+void addItem(){
+  setState(() {
+    //check if input field is not empty
+    //if true add the item in the input field to the item list
+    if(itemController.text.isNotEmpty){
+      itemsToAdd.add(itemController.text);
+      //clear input field after item addition
+      itemController.clear();
+    }
+  });
+}
+//function to clear the item list
+void clearList(){
+  setState(() {
+    //clear entire list
+    itemController.clear();
+  });
+}
 }
